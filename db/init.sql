45d74e6af6179a08cd600652c17d1e50a8427572
charles <codelyfewithcharlie@gmail.com>
2019-11-27 11:38:17 -0700

    updates

---
+++ db/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+username VARCHAR(500) NOT NULL,
+pass VARCHAR(1000) NOT NULL,
+email TEXT NOT NULL UNIQUE,
+startDate NUMBER,
+finishDate NUMBER,
+completed BOOLEAN DEFAULT FALSE)
+
+VALUES(ccamp290, rufus0606, charles.e.campbell1989@gmail.com, 1234, 1234, false)
+
+CREATE TABLE completed(
+id NUMBER,
+movieId NUMBER
+)
--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
+require("dotenv").config();
 const express = require("express");
+const massive = require("massive");
+const session = require("express-session");
+const { PORT, SESSION_STRING, CONNECTION_STRING } = process.env;
 const app = express();
+app.use(express.json());
 
-const PORT = 8293;
+massive(CONNECTION_STRING).then(db => {
+  app.set("db", db);
+  console.log("db connected");
+});
+
+app.use(
+  session({
+    secret: SESSION_STRING,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 13452345235623623456326435636
+    }
+  })
+);
+
 app.listen(PORT, () => {
   console.log("db running");
 });
